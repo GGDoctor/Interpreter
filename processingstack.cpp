ea2a42c93255e250daca92dea900dbc93bc7a037
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2024-05-05 09:51:27 -0700

    Fixed linker error
    Fixed a linker error caused by the makefile not working and the processingstack.cpp file being deleted from this branch. Causing the linker error

---
+++ processingstack.cpp
@@ -0,0 +1,63 @@
+#include "processingstack.hpp"
+#include "RecursiveDescentParser.hpp" // Include RecursiveDescentParser.hpp
+
+void ProcessingStack::Pop()
+{
+    // If the head is null, indicate that the stack is empty
+    if (head == nullptr)
+    {
+        cerr << "Stack is empty and you are trying to pop" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    Processing_Node* temp = head;
+    head = head->next;
+    delete temp;
+}
+
+void ProcessingStack::Push(LCRS* new_data)
+{
+    // Add the new node to the top of the stack
+    Processing_Node* new_node = new Processing_Node(new_data);
+    new_node->next = head;
+    head = new_node;
+}
+
+/*
+
+//Work in progress
+int ProcessingStack::evaluateExpression(const LCRS* node) {
+    // Handle evaluating an expression represented by an AST node
+    if (!node) {
+        cerr << "Invalid AST node" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Assuming NodeType and related definitions are accessible from RecursiveDescentParser.hpp
+    if (node->type == NodeType::Operator) {
+        int operand1 = evaluateExpression(node->leftChild);
+        int operand2 = evaluateExpression(node->rightChild);
+
+        switch (node->token.character) {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                return operand1 / operand2;
+            default:
+                cerr << "Unknown operator: " << node->token.character << endl;
+                exit(EXIT_FAILURE);
+        }
+    } else if (node->type == NodeType::Operand) {
+        // Assuming operand nodes contain integer values
+        return stoi(node->token.character);
+    } else {
+        cerr << "Invalid AST node type" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+*/
